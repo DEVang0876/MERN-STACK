6b9bf2aa1107a9c68c97cc74c68f6e95cac8ae50
Devang <23aiml014@charusat.edu.in>
2025-07-10 10:38:25 +0530

    .js

--- javascript/script.js
+++ javascript/script.js
@@ -14,6 +14,8 @@ else {
     document.writeln("<br><h1><i>Sum is not greater than 20<i></h1>");
 }
 
+
+// Function to greet a user
 function greet(name) {
     document.writeln("<br>Hello, " + name + "!");
 }
@@ -25,3 +27,65 @@ greet("Tony Stark");
 // Let --> Block Scope
 // Const --> Block Scope, Immutable 
 
+for (let i = 0; i < 5; i++) {
+    document.writeln("<br>Current value of i is: " + i);
+}
+
+//table loops
+document.writeln("<br><table border='1'>"); 
+for (let i = 1; i <= 5; i++) {
+    if(i%2 == 0) 
+    {
+        document.writeln("<tr><td>Even: " + i + "</td>");
+        document.writeln("<td>Even: " + i + "</td></tr>");
+        
+    } 
+    else 
+    {
+        document.writeln("<tr><td>Odd: " + i + "</td>");
+        document.writeln("<td>Odd: " + i + "</td></tr>");
+    }
+}
+document.writeln("</table>");
+
+
+//backticks
+var a=100
+document.writeln(`value of var a is ${a}`);
+
+
+
+
+
+//for loop
+for (let i = 0; i < 5; i++) {
+    document.writeln("<br><br><br>Current value of i is: " + i);
+}
+
+//for in loop
+let person = {name: "John", age: 30, city: "New York"};
+document.writeln("<br><br><br>Using for-in loop:");
+for (let key in person) {
+    document.writeln("<br>" + key + ": " + person[key]);
+}
+
+//for of loop
+let fruits = ["Apple", "Banana", "Cherry"];
+document.writeln("<br><br><br>Using for-of loop:");
+for (let fruit of fruits) {
+    document.writeln("<br>" + fruit);
+}
+
+// while loop
+let count = 0;
+document.writeln("<br><br><br>Using while loop:");
+while (count < 5) {
+    document.writeln("<br>Count is: " + count);
+    count++;
+}
+
+//for each loop
+document.writeln("<br><br><br>Using ForEach loop:");
+fruits.forEach(function(fruit) {
+    document.writeln("<br>Fruit: " + fruit);
+});
