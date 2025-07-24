73b1407102bf8e65a1e850db60f85e322d3830c7
Devang <23aiml014@charusat.edu.in>
2025-07-24 16:02:19 +0530

    ok

--- reactapp/src/about.jsx
+++ reactapp/src/about.jsx
@@ -1,10 +1,38 @@
-function about() {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <p>This is the about page of our application.</p>
-    </div>
-  );
+import React from 'react';
+
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { Counter: 0, msg: "King says" };
+  }
+
+  increment = () => {
+    if (this.state.Counter >= 3) {
+      this.setState({ msg: "King says NOt allowed >3" });
+    } else {
+      this.setState({ Counter: this.state.Counter + 1, msg: "" });
+    }
+  };
+
+  decrement = () => {
+    if (this.state.Counter <= 0) {
+      this.setState({ msg: "King says Not allowed <0" });
+    } else {
+      this.setState({ Counter: this.state.Counter - 1, msg: "" });
+    }
+  };
+
+  render() {
+    return (
+      <div className='button'>
+        <h1>Counter Page</h1>
+        <p>Count: {this.state.Counter}</p>
+        <button onClick={this.increment} className=''>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+        <p>{this.state.msg}</p>
+      </div>
+    );
+  }
 }
 
-export default about;
+export default About;
