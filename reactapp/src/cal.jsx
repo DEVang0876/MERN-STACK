5d277a57e6e0a663cf546a26d0ce52e1fc7eed89
Devang <23aiml014@charusat.edu.in>
2025-07-31 16:01:24 +0530

    .updated

--- reactapp/src/cal.jsx
+++ reactapp/src/cal.jsx
@@ -31,13 +31,13 @@ function Calculator() {
             <h2>Calculator</h2>
             num1<input type='text' value={num1} onChange={(e) => setNum1(e.target.value)} placeholder="Enter first number" />
             num2<input type='text' value={num2} onChange={(e) => setNum2(e.target.value)} placeholder="Enter second number" />
-            Choice: <input type="radio" value="add" checked={choice === "add"} onChange={(e) => setChoice(e.target.value)} />Add
-            <input type="radio" value="subtract" checked={choice === "subtract"} onChange={(e) => setChoice(e.target.value)} />Subtract
-            <input type="radio" value="multiply" checked={choice === "multiply"} onChange={(e) => setChoice(e.target.value)} />Multiply
-            <input type="radio" value="divide" checked={choice === "divide"} onChange={(e) => setChoice(e.target.value)} />Divide
+            Choice: <input type="radio" name="choice" value="add"  onChange={(e) => setChoice(e.target.value)} />Add
+            <input type="radio" name="choice" value="subtract"  onChange={(e) => setChoice(e.target.value)} />Subtract
+            <input type="radio" name="choice" value="multiply"  onChange={(e) => setChoice(e.target.value)} />Multiply
+            <input type="radio" name="choice" value="divide"  onChange={(e) => setChoice(e.target.value)} />Divide
             <br />
             <button onClick={calculate}>Calculate</button>
-            {ans !== null && <p>Result: {ans}</p>}
+            {ans }
         </div>   
        
     );
